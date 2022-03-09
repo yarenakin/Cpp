959618abce03cdee3146b93faebaaf38d5167a25
yarenakin <61123017+yarenakin@users.noreply.github.com>
2022-03-09 23:26:11 +0300

    Create bitmask.c

---
+++ bitmask.c
@@ -0,0 +1,18 @@
+/*
+bitsel makseleme işlemi (bitmask)
+by Yaren AKIN
+09.03.2022
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+void mask(int val,int index)
+{
+    val |= 1 << index;
+    printf("val = %d\n", val);
+}
+int main(void)
+{
+    mask(65,5); //65 sayısının 5. biti maskelendi.
+}
