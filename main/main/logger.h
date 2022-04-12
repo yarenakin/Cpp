7fa1118c8c0eae3901b15f4641347105b2dd96ac
yarenakin <61123017+yarenakin@users.noreply.github.com>
2022-04-12 15:40:07 +0300

    Update logger.h

--- main/main/logger.h
+++ main/main/logger.h
@@ -1,3 +1,6 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -7,17 +10,18 @@
 #include <time.h>
 
 enum  class LogLevel { TRACE, DEBUG, INFO, WARN, ERROR };
-#define LOG_DEBUG( msg ) Logger::log(LogLevel::DEBUG, msg, __LINE__, __FILE__, __FUNCTION__ )
+#define LOG_DEBUG( lvl,msg ) Logger::log( lvl, msg, __FILE__, __FUNCTION__, __LINE__ )
 
 class Logger
 {
 public:
     Logger();
-    static void log(LogLevel level, const std::string &message,int line,const char* file, const char* function);
+    static void log(LogLevel level, const std::string &message,const char* file,const char* function,int line);
     static std::string logFileName;
     static bool print;
     static bool save;
     static std::string version;
-    
+
 };
 
+#endif // LOGGER_H
